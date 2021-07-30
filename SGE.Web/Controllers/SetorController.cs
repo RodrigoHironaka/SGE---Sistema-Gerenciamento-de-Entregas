517b02540c8336d56a49baf260eaeafc3d290826
Rodrigo Hironaka <rocknhiro@hotmail.com>
2021-07-30 13:50:04 -0300

    inicio Editar setor web

--- SGE.Web/Controllers/SetorController.cs
+++ SGE.Web/Controllers/SetorController.cs
@@ -1,4 +1,5 @@
-﻿using SGE.Repositorio.Repositorios;
+﻿using SGE.Dominio.Entidades;
+using SGE.Repositorio.Repositorios;
 using SGE.Web.Utilitarios;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.Web.Mvc;
 
 namespace SGE.Web.Controllers
 {
+    [Authorize(Roles = "Administrador")]
     public class SetorController : Controller
     {
         #region Repositorio
@@ -25,11 +27,23 @@ namespace SGE.Web.Controllers
         }
         #endregion
 
+        Setor setor = new Setor();
+
         // GET: Setor
         public ActionResult Index()
         {
             var setores = Repositorio.ObterTodos().ToList();
             return View(setores);
+        }
+
+        public ActionResult Editar(Int64 id = 0)
+        {
+            if(id > 0)
+            {
+                setor = Repositorio.ObterPorId(id);
+
+            }
+            return View(setor);
         }
     }
 }
